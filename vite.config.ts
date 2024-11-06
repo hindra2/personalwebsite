8a0c08b4fab29d7b43b0b12035d8414c9a6da679
H1R0L9 <53500251+H1R0L9@users.noreply.github.com>
2024-11-05 20:34:27 -0600

    fixed deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: './dist'
-  }
+  base: "/personalwebsite/",
+  plugins: [react()]
 })
