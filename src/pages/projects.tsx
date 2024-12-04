9fdd1adea4445fd4c4d61dfa18b95a25324a3ca5
hindra2 <harold.indra@gmail.com>
2024-12-03 21:14:09 -0600

    fixed headings for different sections

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,10 +1,10 @@
 const Projects: React.FC = () => {
     return (
-        <div className="text-3xl h-screen">
-            <span className="text-text">
+        <div className="text-xl flex flex-col mt-10 w-screen h-screen ml-10 text-text">
+            <span className="text-3xl font-bold">
                 Projects
             </span>
         </div>
-    );
-};
+)};
+
 export default Projects
--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -2,12 +2,14 @@ import SkillNetwork from "../components/skill-network";
 
 const Skills: React.FC = () => {
     return (
-        <div className="text-3xl flex flex-col w-full h-screen items-center justify-center">
-            <span className="text-text">
+        <div className="text-xl flex flex-col mt-10 w-screen h-screen ml-10 text-text">
+            <span className="text-3xl font-bold">
                 Skills
             </span>
-            <div className="flex items-center justify-center w-screen text-lg text-text">
-                <SkillNetwork />
+            <div className="text-3xl flex flex-col w-full h-screen items-center justify-center">
+                <div className="flex items-center justify-center w-screen text-lg text-text">
+                    <SkillNetwork />
+                </div>
             </div>
         </div>
     );
