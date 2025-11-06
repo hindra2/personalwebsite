9bd49daa0550c3f1a72ab105b02bf149ca92706a
hindra2 <harold.indra@gmail.com>
2025-11-06 01:59:35 -0600

    changed modal css for mobile friendly

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -43,7 +43,7 @@ export const Modal = ({
               damping: 25,
               stiffness: 300,
             }}
-            className="fixed z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90%] h-[90%] bg-background rounded-lg shadow-2xl flex flex-col"
+            className="fixed z-50 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-[90%] md:h-[90%] w-[95%] h-[95%] bg-background rounded-lg shadow-2xl flex flex-col"
           >
             <button
               onClick={handleClose}
@@ -51,7 +51,7 @@ export const Modal = ({
             >
               <X size={20} />
             </button>
-            <div className="h-full overflow-y-auto p-10 scrollbar scrollbar-thumb-base1 scrollbar-track-background">
+            <div className="h-full overflow-y-auto md:p-10 pt-10 scrollbar scrollbar-thumb-base1 scrollbar-track-background">
               <div className="space-y-4">{content}</div>
             </div>
           </motion.div>
