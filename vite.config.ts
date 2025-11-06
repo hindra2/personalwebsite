8cd0b50347a9fdb518b2b3945cc2f34466b70ce1
hindra2 <harold.indra@gmail.com>
2025-11-06 00:54:04 -0600

    updated imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from "path"
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
